9d0c1d5ccb8163987b62ee8ce230bc245560a576
BlackDP <blackdp@ya.ru>
2023-11-26 15:47:07 +0300

    L2_d_done

--- django_views_routing_homework/views/level_2/d_authorization.py
+++ django_views_routing_homework/views/level_2/d_authorization.py
@@ -39,6 +39,12 @@ def process_authorization_view(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         # код писать тут
+        print(data)
+        login = data.get('username')
+        pwd = data.get('password')
+        if login in USERNAME_TO_PASSWORD_MAPPER and USERNAME_TO_PASSWORD_MAPPER.get(login) == pwd:
+            return JsonResponse(data={}, status=200)
+        return JsonResponse(data={}, status=403)
     else:
         return HttpResponseNotAllowed(permitted_methods=['POST'])
 
